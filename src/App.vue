7616aa68fb3deb51e51ae331aeed856bc0f131c5
T.J <t.ruggles@live.ca>
2018-05-25 03:54:02 -0400

    Added multiple global and server components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+  <div class="container">
+    <appHeader></appHeader>  
+    <hr>
+    <div class="row">
+      <appServers></appServers>
+      <appServerDetails></appServerDetails>
     </div>
+    <hr>
+    <appFooter></appFooter>
+  </div>
 </template>
 
 <script>
+import Header from './components/global/Header.vue';
+import Servers from './components/servers/Servers.vue';
+import ServerDetails from './components/servers/ServerDetails.vue';
+import Footer from './components/global/Footer.vue';
+
+export default {
+  components: {
+    appHeader: Header,
+    appServers: Servers,
+    appServerDetails: ServerDetails,
+    appFooter: Footer
+  }
+}
 </script>
 
 <style>
